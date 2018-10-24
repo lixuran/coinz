724cd1b6bea43884680e21728481450528277fc5
lixuran <mrlixuran@126.com>
2018-10-24 14:00:34 +0100

    ''' finshed all slides and tutorials , copy code from the slides , prepare to get started'''

--- app/src/main/java/com/ilpcoursework/coinz/MainActivity.kt
+++ app/src/main/java/com/ilpcoursework/coinz/MainActivity.kt
@@ -5,21 +5,135 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import android.location.Location
+import android.util.Log
+
+//mapbox import
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
+
+//permissions
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
+
+import com.mapbox.android.core.location.LocationEngine
+import  com.mapbox.android.core.location.LocationEngineListener
+import com.mapbox.android.core.location.LocationEnginePriority
+import com.mapbox.android.core.location.LocationEngineProvider
+import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin
+import com.mapbox.mapboxsdk.plugins.locationlayer.modes.CameraMode
+import com.mapbox.mapboxsdk.plugins.locationlayer.modes.RenderMode
 
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+//drawing the map
+import com.mapbox.geojson.Feature
+import com.mapbox.geojson.FeatureCollection
+import com.mapbox.geojson.Geometry
+import com.mapbox.geojson.Point
+import com.google.gson.JsonObject
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource
+//with marker
+import com.mapbox.mapboxsdk.annotations.Icon
+import com.mapbox.mapboxsdk.annotations.IconFactory
+import com.mapbox.mapboxsdk.annotations.Marker
+import com.mapbox.mapboxsdk.annotations.MarkerOptions
 
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback,
+        LocationEngineListener, PermissionsListener {
+
+    private val tag = "MainActivity"
+    private var mapView: MapView? = null
+    private var map: MapboxMap? = null
+    private lateinit var originLocation: Location
+    private lateinit var permissionsManager: PermissionsManager
+    private lateinit var locationEngine: LocationEngine
+    private lateinit var locationLayerPlugin: LocationLayerPlugin
+    private lateinit final var ACCESS_TOKEN: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        Mapbox.getInstance(this, ACCESS_TOKEN)
+        mapView = findViewById(R.id.mapboxMapView)
+        mapView?.onCreate(savedInstanceState)
+        mapView?.getMapAsync(this)
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
     }
+
+    override fun onMapReady(mapboxMap: MapboxMap?) {
+        if (mapboxMap == null) {
+            Log.d(tag, "[onMapReady] mapboxMap is null")
+        } else {
+            map = mapboxMap
+            // Set user interface options
+            map?.uiSettings?.isCompassEnabled = true
+            map?.uiSettings?.isZoomControlsEnabled = true
+            // Make location information available
+            enableLocation()
+        }
+    }
+
+    private fun enableLocation() {
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            Log.d(tag, "Permissions are granted")
+            initialiseLocationEngine()
+            initialiseLocationLayer()
+        } else {
+            Log.d(tag, "Permissions are not granted")
+            permissionsManager = PermissionsManager(this)
+            permissionsManager.requestLocationPermissions(this)
+        }
+    }
+
+    @SuppressWarnings("MissingPermission")
+    private fun initialiseLocationLayer() {
+        if (mapView == null) {
+            Log.d(tag, "mapView is null")
+        } else {
+            if (map == null) {
+                Log.d(tag, "map is null")
+            } else {
+                locationLayerPlugin = LocationLayerPlugin(mapView!!,
+                        map!!, locationEngine)
+                locationLayerPlugin.apply {
+                    setLocationLayerEnabled(true)
+                    cameraMode = CameraMode.TRACKING
+                    renderMode = RenderMode.NORMAL
+                }
+            }
+        }
+    }
+
+    @SuppressWarnings("MissingPermission")
+    private fun initialiseLocationEngine() {
+        locationEngine = LocationEngineProvider(this)
+                .obtainBestLocationEngineAvailable()
+        locationEngine.apply {
+            interval = 5000 // preferably every 5 seconds
+            fastestInterval = 1000 // at most every second
+            priority = LocationEnginePriority.HIGH_ACCURACY
+                    activate()
+        }
+        val lastLocation = locationEngine.lastLocation
+        if (lastLocation != null) {
+            originLocation = lastLocation
+            setCameraPosition(lastLocation)
+        } else {
+            locationEngine.addLocationEngineListener(this)
+        }
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
